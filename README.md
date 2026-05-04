9c6e45de33d63386f007df3ad5070b98df8ea03d
ryl2005 <ryl010@ucsd.edu>
2026-05-04 15:17:50 -0700

    expose completed

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
 
-Ryan Lee 
+Ryan Lee and Ethan Pham 
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,49 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImg = document.querySelector('#expose > img');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('#expose button');
+  const audio = document.querySelector('#expose audio');
+  const jsConfetti = new JSConfetti();
+
+  audio.volume = Number(volumeSlider.value) / 100;
+
+  hornSelect.addEventListener('change', () => {
+    const horn = hornSelect.value;
+
+    hornImg.src = `assets/images/${horn}.svg`;
+    hornImg.alt = hornSelect.options[hornSelect.selectedIndex].text;
+    audio.src = `assets/audio/${horn}.mp3`;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volume = Number(volumeSlider.value);
+    audio.volume = volume / 100;
+
+    if (volume === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+      volumeIcon.alt = 'Volume level 0';
+    } else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      volumeIcon.alt = 'Volume level 1';
+    } else if (volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+      volumeIcon.alt = 'Volume level 2';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      volumeIcon.alt = 'Volume level 3';
+    }
+  });
+
+  playButton.addEventListener('click', () => {
+    audio.currentTime = 0;
+    audio.play();
+
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
